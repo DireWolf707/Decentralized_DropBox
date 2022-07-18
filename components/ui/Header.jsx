07b682ff8d3335605d165b39490698da2bb16ecf
DireWolf707 <rahulsood707@gmail.com>
2022-07-18 23:13:22 +0530

    added glowing bg behind wallet

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -1,14 +1,27 @@
 import { ConnectButton } from "web3uikit"
+import { motion } from "framer-motion"
 import Link from "next/link"
 
 const Header = () => {
   return (
     <nav className="flex justify-between items-center p-4 mx-2 border-b-2 ">
-      <Link href='/'>
+      <Link href="/">
         <button className="text-xl font-bold">Title</button>
       </Link>
+
       <div>
-        <ConnectButton moralisAuth={false} />
+        <div className="relative">
+          <motion.div
+            animate={{
+              scale: [1.05, 1, 1.05],
+              transition: { repeat: Infinity, duration: 2, repeatType: "mirror" },
+            }}
+            className="absolute inset-x-3 -inset-y-1 blur-sm rounded-2xl bg-gradient-to-br from-pink-600 to bg-purple-600"
+          ></motion.div>
+          <div className="relative">
+            <ConnectButton moralisAuth={false} />
+          </div>
+        </div>
       </div>
     </nav>
   )
