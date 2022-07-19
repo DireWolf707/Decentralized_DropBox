c219c9bb113aef2f6c78486a2479e2bbf964cf50
DireWolf707 <rahulsood707@gmail.com>
2022-07-19 19:01:08 +0530

    added reauth on account change

--- pages/MyFiles/index.jsx
+++ pages/MyFiles/index.jsx
@@ -1,8 +1,17 @@
 import { useMoralis } from "react-moralis"
 import SignUp from "../../components/forms/Signup"
+import React, { useEffect } from "react"
+import { useRouter } from "next/router"
 
 const Root = () => {
-  const { isAuthenticated, user } = useMoralis()
+  const { isAuthenticated, user, Moralis, authenticate } = useMoralis()
+  useEffect(() => {
+    Moralis.onAccountChanged(async (account) => {
+      if (account) {
+        await authenticate()
+      }
+    })
+  }, [])
   if (!isAuthenticated) return <div>connect to metamask</div>
   if (!user.attributes.storageAPI) return <SignUp />
   return <div>registered user</div>
