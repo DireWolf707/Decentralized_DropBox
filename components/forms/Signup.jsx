f94bceed7ef40a854b4d5b77732082ea9139ab61
DireWolf707 <rahulsood707@gmail.com>
2022-07-20 12:51:16 +0530

    added border to signup component

--- components/forms/Signup.jsx
+++ components/forms/Signup.jsx
@@ -1,14 +1,19 @@
 import { useMoralis, useMoralisCloudFunction } from "react-moralis"
 import { useState } from "react"
+import Border from "../animation/Border"
 
 const Signup = () => {
   const [username, setUsername] = useState("")
   const [storageAPI, setStorageAPI] = useState("")
   const { isUserUpdating, refetchUserData } = useMoralis()
-  const { fetch: createNewUser, isLoading } = useMoralisCloudFunction("createNewUser", {
-    username,
-    storageAPI
-  }, { autoFetch: false })
+  const { fetch: createNewUser, isLoading } = useMoralisCloudFunction(
+    "createNewUser",
+    {
+      username,
+      storageAPI,
+    },
+    { autoFetch: false }
+  )
 
   const signup = async (e) => {
     e.preventDefault()
@@ -26,31 +31,33 @@ const Signup = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={signup} className="border rounded flex flex-col w-3/5 mx-auto mt-8 p-4 space-y-2">
-        <h1 className="text-center text-xl">Sign Up</h1>
-        <label>
-          <span className="block">Username :</span>
-          <input
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-full rounded text-black px-2 py-1"
-            type="text"
-          />
-        </label>
-        <label>
-          <span className="block">Storage API Key :</span>
-          <input
-            value={storageAPI}
-            onChange={(e) => setStorageAPI(e.target.value)}
-            className="w-full rounded text-black px-2 py-1"
-            type="text"
-          />
-        </label>
-        <button disabled={isLoading || isUserUpdating} className="w-full border bg-blue-500 p-1 rounded" type="submit">
-          Submit
-        </button>
-      </form>
+    <div className="w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto my-32">
+      <Border padding={"10"}>
+        <form onSubmit={signup} className="flex flex-col space-y-2">
+          <h1 className="text-center text-xl font-bold">Sign Up</h1>
+          <label>
+            <span className="block mb-1">Username :</span>
+            <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full rounded text-blue-600 px-2 py-1"
+              type="text"
+            />
+          </label>
+          <label>
+            <span className="block mb-1">Storage API Key :</span>
+            <input
+              value={storageAPI}
+              onChange={(e) => setStorageAPI(e.target.value)}
+              className="w-full rounded text-blue-600 px-2 py-1"
+              type="text"
+            />
+          </label>
+          <button disabled={isLoading || isUserUpdating} className="w-full bg-blue-500 hover:bg-blue-800 hover:text-slate-300 p-1 rounded" type="submit">
+            Submit
+          </button>
+        </form>
+      </Border>
     </div>
   )
 }
