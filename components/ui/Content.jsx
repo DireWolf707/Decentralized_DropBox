c61fc377f1b831458abbf781a95b793c142268ff
DireWolf707 <rahulsood707@gmail.com>
2022-07-22 18:59:27 +0530

    added table navbar

--- components/ui/Content.jsx
+++ components/ui/Content.jsx
@@ -14,15 +14,6 @@ const Content = ({ folderId, setFolderId }) => {
 
   return (
     <div>
-      <div className="flex divide-x-2 space-x-2">
-        {data &&
-          data.ancestors.map((ancestor) => (
-            <div key={ancestor._id}>
-              <button onClick={() => setFolderId(ancestor._id)}> {ancestor.name}</button>
-            </div>
-          ))}
-      </div>
-
       <form>
         <input value={newFolderName} onChange={(e) => setNewFolderName(e.target.value)} type="text" className="text-black" />
         <button
@@ -40,14 +31,16 @@ const Content = ({ folderId, setFolderId }) => {
 
       <div className="flex p-5 space-x-4">
         <div className="w-1/5 min-h-[80vh] bg-slate-600"></div>
+
         <div className="w-4/5">
           <div className="p-2 bg-gradient-to-br from-slate-200 to-indigo-500 rounded-xl">
             <div className="opacity-95 space-y-1 rounded-lg">
-            {contentLoading && <div>loading....</div>}
-            <Table data={data} setFolderId={setFolderId} />
+              {!data && <div className="text-3xl">Loading...</div>}
+              {data && <Table data={data} setFolderId={setFolderId} contentLoading={contentLoading} />}
             </div>
           </div>
         </div>
+        
       </div>
     </div>
   )
--- components/ui/Table.jsx
+++ components/ui/Table.jsx
@@ -2,6 +2,40 @@ import DataTable from "react-data-table-component"
 
 const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>
 
+const HeaderComponent = ({ ancestors, currFolder, setFolderId }) => {
+  return (
+    <div className="flex text-slate-200 text-sm font-medium">
+      {ancestors.length >= 7 && <span className="mx-2 text-blue-400">... /</span>}
+      {[...ancestors].slice(-6).map((folder) => (
+        <div key={folder._id}>
+          <button onClick={() => setFolderId(folder._id)}>
+            {folder.name.length <= 15 ? folder.name : folder.name.substr(0, 15) + "..."}
+          </button>
+          <span className="mx-2 text-blue-400">/</span>
+        </div>
+      ))}
+      <div key={currFolder._id}>
+        <div className="underline underline-offset-2 text-yellow-400 pointer-events-none">
+          {currFolder.name.length <= 15 ? currFolder.name : currFolder.name.substr(0, 15) + "..."}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const NoDataComponent = () => {
+  return <p className="py-8">Folder is Empty!</p>
+}
+
+const SubHeaderComponent = () => {
+  return (
+    <div className="flex justify-between w-full">
+      <div>icons</div>
+      <div>Search</div>
+    </div>
+  )
+}
+
 const columns = [
   {
     name: "Name",
@@ -36,43 +70,39 @@ const columns = [
   },
 ]
 
-const Table = ({ data, setFolderId }) => {
+const Table = ({ data, setFolderId, contentLoading }) => {
   return (
     <>
-      {data && (
-        <DataTable
-          columns={columns}
-          data={data.folders}
-          title={"My Files"}
+      <DataTable
+        columns={columns}
+        data={data.folders}
+        onRowClicked={(row) => setFolderId(row._id)}
+        title={
+          <HeaderComponent ancestors={data.ancestors} setFolderId={setFolderId} currFolder={{ _id: data.objectId, name: data.name }} />
+        }
+        expandableRowsComponent={ExpandedComponent}
+        //expandableRowsComponentProps
+        expandableRows={true}
+        noDataComponent={<NoDataComponent />}
+        persistTableHead={true}
+        fixedHeader={true}
+        subHeader={true}
+        subHeaderComponent={<SubHeaderComponent />}
+        highlightOnHover={true}
+        pointerOnHover={true}
+        theme="dark"
+        keyField={"_id"}
+        progressPending={contentLoading}
+        //progressComponent
 
-          expandableRowsComponent={ExpandedComponent}
-          //expandableRowsComponentProps
-          onRowClicked={row => setFolderId(row._id)}
-          expandableRows={true}
-          //noDataComponent
+        // noContextMenu={false}
+        // contextMessage
+        // contextActions
 
-          persistTableHead={true}
-          fixedHeader={true}
-          subHeader={true}
-          // subHeaderComponent
-
-          highlightOnHover={true}
-          pointerOnHover={true}
-          theme="dark"
-          keyField={"_id"}
-          
-          //progressPending
-          //progressComponent
-
-          // noContextMenu={false}
-          // contextMessage
-          // contextActions
-
-          //selectableRows={true}
-          //selectableRowsHighlight={true}
-          //selectableRowsRadio={"radio"}
-        />
-      )}
+        //selectableRows={true}
+        //selectableRowsHighlight={true}
+        //selectableRowsRadio={"radio"}
+      />
     </>
   )
 }
