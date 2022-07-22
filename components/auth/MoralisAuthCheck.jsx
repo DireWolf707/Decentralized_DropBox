ceb14f2d8427859cd6089379738167f130aee7e5
DireWolf707 <rahulsood707@gmail.com>
2022-07-22 14:04:46 +0530

    fixed display connect metamask condition

--- components/auth/MoralisAuthCheck.jsx
+++ components/auth/MoralisAuthCheck.jsx
@@ -4,14 +4,14 @@ import { useEffect } from "react"
 import ConnectMetamask from "../ui/ConnectMetamask"
 
 const MoralisAuthCheck = ({ children }) => {
-  const { isAuthenticated, user, Moralis, logout } = useMoralis()
+  const { isWeb3Enabled, isUnauthenticated, user, Moralis, logout } = useMoralis()
   useEffect(() => {
     const unsub = Moralis.onAccountChanged(async (account) => {
       if (account) await logout()
     })
     return unsub
   }, [])
-  if (!isAuthenticated) return <ConnectMetamask />
+  if (!isWeb3Enabled || isUnauthenticated) return <ConnectMetamask />
   if (!user.attributes.storageAPI) return <SignUp />
   return children
 }
