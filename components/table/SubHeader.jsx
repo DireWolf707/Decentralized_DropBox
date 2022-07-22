a3380ea650242fac6c3ec37c44dd35bcf1e7dfea
DireWolf707 <rahulsood707@gmail.com>
2022-07-22 19:18:36 +0530

    added hero-icons

--- components/table/SubHeader.jsx
+++ components/table/SubHeader.jsx
@@ -1,25 +1,28 @@
 import { createNewFolder } from "../../hooks/createNewFolder"
 import { useState } from "react"
+import { FolderAddIcon, RefreshIcon, HomeIcon, DocumentAddIcon } from "@heroicons/react/solid"
 
 const SubHeader = () => {
   const [newFolderName, setNewFolderName] = useState("")
   const { addFolder, loading: folderLoading } = createNewFolder()
   return (
     <div className="flex justify-between w-full">
-      <div>
+      <div className="flex space-x-2">
+        <HomeIcon className="h-6 w-6" />
+        <RefreshIcon className="h-6 w-6" />
+        <DocumentAddIcon className="h-6 w-6" />
         <form>
-          <input value={newFolderName} onChange={(e) => setNewFolderName(e.target.value)} type="text" className="text-black" />
-          <button
+          {/* <input value={newFolderName} onChange={(e) => setNewFolderName(e.target.value)} type="text" className="text-black" /> */}
+          <FolderAddIcon
             disabled={folderLoading}
             onClick={async (e) => {
               e.preventDefault()
               await addFolder(newFolderName, folderId)
               await fetchContent(folderId)
             }}
-            className="border rounded"
-          >
-            Add folder +
-          </button>
+            className="h-6 w-6"
+            role="button"
+          />
         </form>
       </div>
       <div>Search</div>
