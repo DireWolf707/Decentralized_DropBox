44e1cca561983f330c1f692c33f42e1200873a65
DireWolf707 <rahulsood707@gmail.com>
2022-07-23 18:22:08 +0530

    ui button logic update for file upload

--- components/table/SubHeader.jsx
+++ components/table/SubHeader.jsx
@@ -1,4 +1,5 @@
 import { createNewFolder } from "../../hooks/createNewFolder"
+import { createNewFile } from "../../hooks/createNewFile"
 import { useState } from "react"
 import { FolderAddIcon, RefreshIcon, HomeIcon, DocumentAddIcon, FolderIcon, DocumentTextIcon } from "@heroicons/react/solid"
 import { useMoralis } from "react-moralis"
@@ -7,7 +8,10 @@ import { Popover } from "@headlessui/react"
 
 const SubHeader = ({ setFolderId, folderId, fetchCurrFolder }) => {
   const [newFolderName, setNewFolderName] = useState("")
+  const [selectedFile, setSelectedFile] = useState(null)
+
   const { addFolder } = createNewFolder()
+  const { addFile } = createNewFile()
   const { user } = useMoralis()
 
   const addNewFolder = async (close) => {
@@ -17,12 +21,19 @@ const SubHeader = ({ setFolderId, folderId, fetchCurrFolder }) => {
     await fetchCurrFolder()
   }
 
+  const addNewFile = async (close) => {
+    close()
+    setSelectedFile(null)
+    await addFile(selectedFile, folderId)
+  }
+
   return (
     <div className="flex justify-between w-full">
       <div className="flex item-center space-x-2">
         <motion.div whileTap={{ scale: 0.75 }}>
           <HomeIcon onClick={() => setFolderId(user.attributes.rootFolderId)} className="h-7 w-7 text-green-300" role="button" />
         </motion.div>
+
         <motion.div whileTap={{ rotateZ: -360 }}>
           <RefreshIcon onClick={() => fetchCurrFolder()} className="h-7 w-7 text-indigo-300" role="button" />
         </motion.div>
@@ -38,6 +49,7 @@ const SubHeader = ({ setFolderId, folderId, fetchCurrFolder }) => {
             {({ close }) => (
               <div className="flex items-center space-x-2 bg-zinc-500 py-2 p-3 border rounded-full border-zinc-900">
                 <input
+                  onChange={(e) => setSelectedFile(e.target.files[0])}
                   type="file"
                   className="text-sm file:mr-5 file:py-2 file:px-4
                   file:rounded-full file:border-0
@@ -46,7 +58,11 @@ const SubHeader = ({ setFolderId, folderId, fetchCurrFolder }) => {
                   hover:file:bg-violet-100"
                 />
                 <motion.div whileTap={{ scale: 0.75 }}>
-                  <DocumentAddIcon onClick={() => {close()}} className="h-9 w-9 text-white bg-slate-400 p-1 rounded-full" role="button" />
+                  <DocumentAddIcon
+                    onClick={() => addNewFile(close)}
+                    className="h-9 w-9 text-white bg-slate-400 p-1 rounded-full"
+                    role="button"
+                  />
                 </motion.div>
               </div>
             )}
