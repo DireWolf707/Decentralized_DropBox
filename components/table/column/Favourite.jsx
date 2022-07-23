7b0c9028dd85da062fa5ea656d5c4dab1963802b
DireWolf707 <rahulsood707@gmail.com>
2022-07-24 00:37:14 +0530

    added Favourite column component

---
+++ components/table/column/Favourite.jsx
@@ -0,0 +1,30 @@
+import { StarIcon as StarIconOutline } from "@heroicons/react/outline"
+import { StarIcon as StarIconSolid } from "@heroicons/react/solid"
+import { useState } from "react"
+import { markFileAsFav } from "../../../hooks/markFileAsFav"
+
+const Favourite = ({ row }) => {
+  const { fetch } = markFileAsFav()
+  const [marked, setMarked] = useState(row.favourite)
+
+  const toggleFav = async () => {
+    try {
+      const newMarked = await fetch({ params: { fileId: row._id } })
+      setMarked(newMarked)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return row.type ? (
+    marked ? (
+      <StarIconSolid onClick={toggleFav} className="h-5 w-5" role="button" />
+    ) : (
+      <StarIconOutline onClick={toggleFav} className="h-5 w-5" role="button" />
+    )
+  ) : (
+    ""
+  )
+}
+
+export default Favourite
