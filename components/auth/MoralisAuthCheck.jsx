88a2249ae279aef88413402be47a7e8608fb9fb9
DireWolf707 <rahulsood707@gmail.com>
2022-07-24 19:59:08 +0530

    added Loading in MoralisAuth

--- components/auth/MoralisAuthCheck.jsx
+++ components/auth/MoralisAuthCheck.jsx
@@ -2,16 +2,25 @@ import { useMoralis } from "react-moralis"
 import SignUp from "../forms/Signup"
 import { useEffect } from "react"
 import ConnectMetamask from "../ui/ConnectMetamask"
+import Loading from "../animation/Loading"
 
 const MoralisAuthCheck = ({ children }) => {
-  const { isWeb3Enabled, isUnauthenticated, user, Moralis, logout } = useMoralis()
+  const { isUnauthenticated, user, Moralis, logout, isInitializing, isLoggingOut } = useMoralis()
+
   useEffect(() => {
     const unsub = Moralis.onAccountChanged(async (account) => {
       if (account) await logout()
     })
     return unsub
   }, [])
-  if (!isWeb3Enabled || isUnauthenticated) return <ConnectMetamask />
+
+  if (isInitializing || isLoggingOut)
+    return (
+      <div className="absolute z-10 top-[50%] -translate-y-[50%] left-[50%] -translate-x-[50%]">
+        <Loading />
+      </div>
+    )
+  if (isUnauthenticated || !user) return <ConnectMetamask />
   if (!user.attributes.storageAPI) return <SignUp />
   return children
 }
