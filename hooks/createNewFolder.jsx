9a67d47fe4bc2664823919726031ac02b4513b8b
DireWolf707 <rahulsood707@gmail.com>
2022-07-26 14:29:07 +0530

    added notification + validation in creatNewFolder hook

--- hooks/createNewFolder.jsx
+++ hooks/createNewFolder.jsx
@@ -1,12 +1,35 @@
 import { useMoralisCloudFunction } from "react-moralis"
+import { useNotification } from "web3uikit"
 
 export const createNewFolder = () => {
+  const dispatch = useNotification()
   const { fetch, isLoading, isFetching } = useMoralisCloudFunction("createNewFolder", {}, { autoFetch: false })
 
   const addFolder = async (name, parentId) => {
+    // validate folder name
+    if (name.length == 0)
+      return dispatch({
+        type: "warning",
+        message: "Folder name must be atleast 1 character",
+        title: "Warning !",
+        position: "topR",
+      })
+    // call cloud function to create folder
     await fetch({
-      onSuccess: () => console.log("folder added"),
-      onError: (err) => console.log(err),
+      onSuccess: () =>
+        dispatch({
+          type: "success",
+          message: "New folder created successfully",
+          title: "Success !",
+          position: "topR",
+        }),
+      onError: (err) =>
+        dispatch({
+          type: "warning",
+          message: "Something went wrong",
+          title: "Warning !",
+          position: "topR",
+        }),
       params: { name, parentId },
     })
   }
