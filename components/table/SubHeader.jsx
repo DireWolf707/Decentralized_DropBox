4a0d6041e73b87c6774c1f5a8c6f184da0f13042
DireWolf707 <rahulsood707@gmail.com>
2022-07-27 17:51:15 +0530

    tailwindcss fix

--- components/table/SubHeader.jsx
+++ components/table/SubHeader.jsx
@@ -5,22 +5,19 @@ import NewFolder from "./subheaderButton/NewFolder"
 
 const SubHeader = ({ setFolderId, folderId, fetchCurrFolder, rootFolderId }) => {
   return (
-    <div className="flex justify-between w-full">
-      <div className="flex item-center space-x-2">
-        {/* home */}
-        <motion.div whileTap={{ scale: 0.75 }}>
-          <HomeIcon onClick={() => setFolderId(rootFolderId)} className="h-7 w-7 text-green-300" role="button" />
-        </motion.div>
-        {/* refresh */}
-        <motion.div whileTap={{ rotateZ: -360 }}>
-          <RefreshIcon onClick={() => fetchCurrFolder()} className="h-7 w-7 text-indigo-300" role="button" />
-        </motion.div>
-        {/* new file */}
-        <NewFile folderId={folderId} fetchCurrFolder={fetchCurrFolder} />
-        {/* new folder  */}
-        <NewFolder folderId={folderId} fetchCurrFolder={fetchCurrFolder} />
-      </div>
-      <div>Search</div>
+    <div className="flex item-center space-x-2">
+      {/* home */}
+      <motion.div whileTap={{ scale: 0.75 }}>
+        <HomeIcon onClick={() => setFolderId(rootFolderId)} className="h-7 w-7 text-green-300" role="button" />
+      </motion.div>
+      {/* refresh */}
+      <motion.div whileTap={{ rotateZ: -360 }}>
+        <RefreshIcon onClick={fetchCurrFolder} className="h-7 w-7 text-indigo-300" role="button" />
+      </motion.div>
+      {/* new file */}
+      <NewFile folderId={folderId} fetchCurrFolder={fetchCurrFolder} />
+      {/* new folder  */}
+      <NewFolder folderId={folderId} fetchCurrFolder={fetchCurrFolder} />
     </div>
   )
 }
